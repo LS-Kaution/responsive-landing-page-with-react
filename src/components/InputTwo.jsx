08672890d3481c2df607e604e86c8392806a9207
LS-Kaution <leonardosaturnoplaneta@gmail.com>
2023-06-08 17:18:14 -0400

    Supabase installed, logic to registration done

--- src/components/InputTwo.jsx
+++ src/components/InputTwo.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 
-export default function InputTwo({ id, title, link, ancor }) {
+export default function InputTwo({ id, data, check, title, link, ancor }) {
     return (
         <div className={id}>
-            <label htmlFor=""><input type="checkbox" />{title}</label>
+            <label htmlFor=""><input type="checkbox" onChange={data} checked={check} />{title}</label>
             <Link to={link}>{ancor}</Link>
         </div>
     )
--- src/routes/RegistrationPage.jsx
+++ src/routes/RegistrationPage.jsx
@@ -7,15 +7,22 @@ export default function RegistrationPage() {
     const [regnickname, setRegnickname] = useState("")
     const [regemail, setRegemail] = useState("")
     const [regpassword, setRegpassword] = useState("")
+    const [regterms, setTerms] = useState(false)
+    const [regerror, setError] = useState(false)
 
     const noSubmit = (e) => {
         e.preventDefault()
+
+        if (regnickname === "" || regemail === "" || regpassword === "" || regterms === false) {
+            setError(true)
+            return
+        }
+        setError(false)
     }
 
-    function handleChange(evento) {
-        setRegnickname(evento.target.value)
-        console.log(regnickname)
-    }
+    const handleChange = () => {
+        setTerms(!regterms);
+      }
 
     return (
         <section>
@@ -23,16 +30,18 @@ export default function RegistrationPage() {
             <form onSubmit={noSubmit}>
                 <h2>Registration</h2>
 
-                <InputOne type='text' value={regnickname} data={handleChange} title='Enter a Nickname' icon={<ion-icon name="person-circle"></ion-icon>} />
+                <InputOne type='text' value={regnickname} data={e => setRegnickname(e.target.value)} title='Enter a Nickname' icon={<ion-icon name="person-circle"></ion-icon>} />
 
-                <InputOne type='email' value={regemail} data={handleChange} title='Enter a Email' icon={<ion-icon name="mail"></ion-icon>} />
+                <InputOne type='email' value={regemail} data={e => setRegemail(e.target.value)} title='Enter a Email' icon={<ion-icon name="mail"></ion-icon>} />
 
-                <InputOne type='password' value={regpassword} data={handleChange} title='Enter a Password' icon={<ion-icon name="lock-closed"></ion-icon>} />
+                <InputOne type='password' value={regpassword} data={e => setRegpassword(e.target.value)} title='Enter a Password' icon={<ion-icon name="lock-closed"></ion-icon>} />
 
-                <InputTwo id='remember-forgot' title='I agree to the terms & conditions' />
+                <InputTwo id='remember-forgot' data={handleChange} check={regterms} title='I agree to the terms & conditions' />
+                
                 <button type='submit'>Sign Up</button>
 
                 <InputTwo id='login-register' title="Already have an account?" link='/login' ancor='Sign In' />
+                {regerror && <p>All fields are necessary</p>}
             </form>
         </section>
     )
