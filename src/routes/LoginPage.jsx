fa6887328f2eb1ad23d18af5da1551d5f1414bb4
LS-Kaution <leonardosaturnoplaneta@gmail.com>
2023-06-09 17:11:15 -0400

    Logic to login done

--- src/routes/LoginPage.jsx
+++ src/routes/LoginPage.jsx
@@ -3,18 +3,41 @@ import '../style/HeaderLogin.scss'
 import HeaderLogin from '../components/HeaderLogin'
 import InputOne from '../components/InputOne'
 import InputTwo from '../components/InputTwo'
+import { useState } from 'react'
 
 export default function LoginPage() {
+    const [loginnickname, setLoginnickname] = useState('')
+    const [loginpassword, setLoginpassword] = useState('')
+    const [loginremember, setRemember] = useState(false)
+    const [loginerror, setError] = useState(false)
+
+    function noSubmitLogin(e) {
+        e.preventDefault()
+        if (loginnickname === "" || loginpassword === "") {
+            setError(true)
+            return
+        }
+        setError(false)
+        setLoginnickname('')
+        setLoginpassword('')
+        setRemember(false)
+    }
+
+    const handleChangeLogin = () => {
+        setRemember(!loginremember);
+    }
+
     return (
         <section>
             <HeaderLogin />
-            <form action="#">
+            <form onSubmit={noSubmitLogin}>
                 <h2>Login</h2>
-                <InputOne type='text' title='Nickname' icon={<ion-icon name="person-circle"></ion-icon>} />
-                <InputOne type='password' title='Password' icon={<ion-icon name="lock-closed"></ion-icon>} />
-                <InputTwo id='remember-forgot' title='Remember me' ancor='Forgot Password?' />
+                <InputOne type='text' value={loginnickname} data={e => setLoginnickname(e.target.value)} title='Nickname' icon={<ion-icon name="person-circle"></ion-icon>} />
+                <InputOne type='password' value={loginpassword} data={e => setLoginpassword(e.target.value)} title='Password' icon={<ion-icon name="lock-closed"></ion-icon>} />
+                <InputTwo id='remember-forgot' data={handleChangeLogin} check={loginremember} title='Remember me' ancor='Forgot Password?' />
                 <button type='submit'>Sign In</button>
                 <InputTwo id='login-register' title="Don't have an account?" link='/registration' ancor='Sign Up' />
+                {loginerror && <p>*** All fields are necessary ***</p>}
             </form>
         </section>
     )
--- src/routes/RegistrationPage.jsx
+++ src/routes/RegistrationPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { supabase } from '../backend/client'
-import { Link } from "react-router-dom";
 import HeaderLogin from '../components/HeaderLogin'
 import InputOne from '../components/InputOne'
 import InputTwo from '../components/InputTwo'
@@ -12,7 +11,7 @@ export default function RegistrationPage() {
     const [regterms, setTerms] = useState(false)
     const [regerror, setError] = useState(false)
 
-    async function noSubmit(e) {
+    async function noSubmitReg(e) {
         e.preventDefault()
         try {
             const { data, error } = await supabase.auth.signUp({
@@ -29,6 +28,10 @@ export default function RegistrationPage() {
                 return
             }
             setError(false)
+            setRegnickname('')
+            setRegemail('')
+            setRegpassword('')
+            setTerms(false)
             alert('Check your email for verification link')
         }
         catch (error) {
@@ -37,25 +40,20 @@ export default function RegistrationPage() {
 
     }
 
-    const handleChange = () => {
+    const handleChangeReg = () => {
         setTerms(!regterms);
     }
+
     return (
         <section>
             <HeaderLogin />
-            <form onSubmit={noSubmit}>
+            <form onSubmit={noSubmitReg}>
                 <h2>Registration</h2>
-
                 <InputOne type='text' value={regnickname} data={e => setRegnickname(e.target.value)} title='Enter a Nickname' icon={<ion-icon name="person-circle"></ion-icon>} />
-
                 <InputOne type='email' value={regemail} data={e => setRegemail(e.target.value)} title='Enter a Email' icon={<ion-icon name="mail"></ion-icon>} />
-
                 <InputOne type='password' value={regpassword} data={e => setRegpassword(e.target.value)} title='Enter a Password' icon={<ion-icon name="lock-closed"></ion-icon>} />
-
-                <InputTwo id='remember-forgot' data={handleChange} check={regterms} title='I agree to the terms & conditions' />
-
+                <InputTwo id='remember-forgot' data={handleChangeReg} check={regterms} title='I agree to the terms & conditions' />
                 <button type='submit'>Sign Up</button>
-
                 <InputTwo id='login-register' title="Already have an account?" link='/login' ancor='Sign In' />
                 {regerror && <p>*** All fields are necessary ***</p>}
             </form>
